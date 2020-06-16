63d6631a82f3172a4d0e94fcb945e60fa3ca88f8
gszundy <gszundy@gmail.com>
2020-06-15 21:11:10 -0300

    5 files modified, 3 added

---
+++ src/app-toolbar-component/app-toolbar-component.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-app-toolbar-component',
+  templateUrl: './app-toolbar-component.component.html',
+  styleUrls: ['./app-toolbar-component.component.css']
+})
+export class AppToolbarComponentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,21 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { FormsModule } from '@angular/forms';
-
+import { MatSliderModule } from '@angular/material/slider';
+import { MatToolbarModule } from  '@angular/material/toolbar';
+import { MatIconModule } from '@angular/material/icon';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatListModule } from '@angular/material/list';
+import { MatButtonModule } from '@angular/material/button';
+/*
+ MatIconModule, MatSidenavModule, MatListModule, MatButtonModule
+*/
 import { AppComponent } from './app.component';
 import { HelloComponent } from './hello.component';
 
 @NgModule({
-  imports:      [ BrowserModule, FormsModule ],
+  imports:      [ BrowserModule, BrowserAnimationsModule, FormsModule, MatSliderModule, MatToolbarModule, MatIconModule, MatSidenavModule, MatListModule, MatButtonModule ],
   declarations: [ AppComponent, HelloComponent ],
   bootstrap:    [ AppComponent ]
 })
